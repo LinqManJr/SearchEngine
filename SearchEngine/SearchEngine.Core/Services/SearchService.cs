a583ee437deff42330a5b1d3c9026b2f4293214e
LinqManJr <johnybond32@yandex.ru>
2019-12-13 23:25:12 +0900

    add search service

---
+++ SearchEngine/SearchEngine.Core/Services/SearchService.cs
@@ -0,0 +1,36 @@
+﻿using SearchEngine.Core.Engines;
+using SearchEngine.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchEngine.Core.Services
+{
+    public class SearchService : ISearchService
+    {
+        private readonly IEnumerable<ISearchEngine> _engines;
+
+        public SearchService(params ISearchEngine[] engines)
+        {
+            _engines = engines;
+        }
+
+        public async Task<SearchResult> SearchInManyAsync(string pattern)
+        {
+            if (_engines == null)
+                throw new Exception();
+
+            var tasks = _engines.Select(x => x.SearchAsync(pattern));
+            var firstTask = await Task.WhenAny(tasks);
+            return await firstTask;
+        }
+
+        public async Task<SearchResult> SearchInManyAsync(string pattern, params ISearchEngine[] engines)
+        {
+            var tasks = engines.Select(x => x.SearchAsync(pattern));
+            var firstTask = await Task.WhenAny(tasks);
+            return await firstTask;
+        }
+    }
+}
