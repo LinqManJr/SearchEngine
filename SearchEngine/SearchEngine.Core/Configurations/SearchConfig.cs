9435f9cef6e9ac06e49b37aff2448c7eef140c2f
LinqManJr <johnybond32@yandex.ru>
2019-12-13 23:21:30 +0900

    add searchconfig

---
+++ SearchEngine/SearchEngine.Core/Configurations/SearchConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace SearchEngine.Core.Configurations
+{
+    public class SearchConfig
+    {
+        public string SearchEngine { get; set; }
+        public string ApiKey { get; set; }
+        public string AppId { get; set; } // for google
+        public string Url { get; set; }
+        public string UserName { get; set; } = string.Empty; // only yandex
+    }
+}
