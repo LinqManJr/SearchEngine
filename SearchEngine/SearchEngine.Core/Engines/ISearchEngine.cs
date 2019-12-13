33e57c444b0d2bd6bae61d2a89c1ba7a0ddbce94
LinqManJr <johnybond32@yandex.ru>
2019-12-13 23:18:05 +0900

    add interface ISearchEngine

---
+++ SearchEngine/SearchEngine.Core/Engines/ISearchEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SearchEngine.Core.Engines
+{
+    public interface ISearchEngine
+    {
+        SearchResult Search(string pattern);
+
+        Task<SearchResult> SearchAsync(string pattern);
+        
+        //TODO: think about config if we have many Options
+        //TODO: config builder to SearchEngines        
+        
+    }
+}
