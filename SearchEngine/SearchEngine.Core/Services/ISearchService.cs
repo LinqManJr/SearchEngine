48ea0e681e8100b229a0a2b164e177efbee84017
LinqManJr <johnybond32@yandex.ru>
2019-12-13 23:24:48 +0900

    add ISearchService

---
+++ SearchEngine/SearchEngine.Core/Services/ISearchService.cs
@@ -0,0 +1,12 @@
+﻿using SearchEngine.Core.Engines;
+using SearchEngine.Core.Models;
+using System.Threading.Tasks;
+
+namespace SearchEngine.Core.Services
+{
+    public interface ISearchService
+    {
+        public Task<SearchResult> SearchInManyAsync(string pattern);
+        public Task<SearchResult> SearchInManyAsync(string pattern, params ISearchEngine[] engines);
+    }
+}
