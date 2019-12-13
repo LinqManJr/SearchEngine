ccc3d71bacfbd9443f2baec7302d488c57b436da
LinqManJr <johnybond32@yandex.ru>
2019-12-13 23:23:46 +0900

    add extensions

---
+++ SearchEngine/SearchEngine.Core/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using SearchEngine.Core.Configurations;
+using System;
+
+namespace SearchEngine.Core
+{
+    public static class Extensions
+    {  
+        public static string ToYandexSearchUrl(this SearchConfig config, string searchPattern)
+        {
+            string url = string.Concat(config.Url, $"?user={config.UserName}", "&key=", config.ApiKey);
+            return string.Concat(url, "&query=", Uri.EscapeDataString(searchPattern));
+        }
+    }
+}
