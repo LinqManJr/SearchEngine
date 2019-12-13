4290e5f50c9e840836d78994e59825cb6beb5268
LinqManJr <johnybond32@yandex.ru>
2019-12-13 23:29:09 +0900

    add bing search test

---
+++ SearchEngine/SearchEngine.Tests/BingSearchEngineTest.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using SearchEngine.Core.Configurations;
+using SearchEngine.Core.Engines;
+using System.Threading.Tasks;
+
+namespace SearchEngine.Test
+{
+    [TestFixture]
+    public class BingSearchEngineTest
+    {
+        private SearchConfig _config;
+        private ISearchEngine _engine;
+        [SetUp]
+        public void SetUp()
+        {
+            _config = new SearchConfig { ApiKey = "2efb912d79e84eb6820192d1805fb44b", Url = "https://api.cognitive.microsoft.com/bing/v7.0/search" };            
+        }
+
+        [Test]
+        public void ShouldReturnResult()
+        {
+            _engine = new BingSearchEngine(_config);
+            var result = _engine.Search("bing");
+
+            Assert.IsNull(result.Error);
+            Assert.That(result.Results.Count > 0);
+        }
+
+        [Test]
+        public void ShouldReturnErrorResult()
+        {
+            _config.ApiKey = "2ef785999ce24554b5454343e32211";
+            _engine = new BingSearchEngine(_config);
+            var result = _engine.Search("bing");
+
+            Assert.IsNotNull(result.Error);
+            Assert.That(result.Error.Title == "ProtocolError");
+        }
+
+        [Test]
+        public async Task ShouldReturnResultAsync()
+        {
+            _engine = new BingSearchEngine(_config);
+            var result = await _engine.SearchAsync("bing");
+
+            Assert.IsNull(result.Error);
+            Assert.That(result.Results.Count > 0);
+        }
+
+        [Test]
+        public async Task ShouldReturnErrorResultAsync()
+        {
+            _config.ApiKey = "2ef785999ce24554b5454343e32211";
+            _engine = new BingSearchEngine(_config);
+            var result = await _engine.SearchAsync("bing");
+
+            Assert.IsNotNull(result.Error);
+            Assert.That(result.Error.Title == "ProtocolError");
+        }
+    }
+}
