93c93702e0e066109b7a88f9fc4467232b70fa91
LinqManJr <johnybond32@yandex.ru>
2019-12-14 13:05:24 +0900

    Add SearchContext add nuget efcore sqlserver

---
+++ SearchEngine/SearchEngine.Domain/Context/SearchContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SearchEngine.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchEngine.Domain.Context
+{
+    public class SearchContext : DbContext
+    {
+        public SearchContext(DbContextOptions<SearchContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<Result> Results { get; set; }
+    }
+}
