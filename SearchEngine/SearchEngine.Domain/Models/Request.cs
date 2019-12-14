a1fa651c17f5720214692b5eeb4726823287a64c
LinqManJr <johnybond32@yandex.ru>
2019-12-14 11:05:50 +0900

    add models Request, Result

---
+++ SearchEngine/SearchEngine.Domain/Models/Request.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchEngine.Domain.Models
+{
+    public class Request
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Engine { get; set; }
+        public int ResultId { get; set; }
+        public virtual ICollection<Result> Results { get; set; }
+    }
+}
---
+++ SearchEngine/SearchEngine.Domain/Models/Result.cs
@@ -0,0 +1,11 @@
+﻿namespace SearchEngine.Domain.Models
+{
+    public class Result
+    {
+        public int Id { get; set; }
+        public int ItemsCount { get; set; }
+        public Request Request { get; set; }
+
+
+    }
+}
