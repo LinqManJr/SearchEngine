fdf47785a98f5968771f797473929152b869a564
LinqManJr <johnybond32@yandex.ru>
2019-12-15 01:42:10 +0900

    +

--- SearchEngine/SearchEngine.Core/Extensions.cs
+++ SearchEngine/SearchEngine.Core/Extensions.cs
@@ -4,12 +4,7 @@ using System;
 namespace SearchEngine.Core
 {
     public static class Extensions
-    {  
-        public static string ToYandexSearchUrl(this SearchConfig config, string searchPattern)
-        {
-            string url = string.Concat(config.Url, $"?user={config.UserName}", "&key=", config.ApiKey);
-            return string.Concat(url, "&query=", Uri.EscapeDataString(searchPattern));
-        }
+    {       
         public static string ToYandexSearchUrl(this YandexSearchOptions options, string searchPattern)
         {
             string url = string.Concat(options.Uri, $"?user={options.Username}", "&key=", options.Apikey);
