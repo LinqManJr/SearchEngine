b11a781dd972f381d98863a3fef0c6b843758c67
LinqManJr <johnybond32@yandex.ru>
2019-12-15 00:29:14 +0900

    add data to config engines to appsettings

--- SearchEngine/SearchEngine.WebApp/Services/SearchServiceFactory.cs
+++ SearchEngine/SearchEngine.WebApp/Services/SearchServiceFactory.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using SearchEngine.Core.Engines;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace SearchEngine.Core.Services
+namespace SearchEngine.WebApp.Services
 {
-    class SearchServiceFactory
+    public class SearchServiceFactory
     {
+        private readonly IConfiguration _configuration;
+
+        public SearchServiceFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IEnumerable<ISearchEngine> GetSearchEngines()
+        {
+            var engines = new List<ISearchEngine>();
+
+            return engines;
+        }
     }
 }
