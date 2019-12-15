689c5267a3db2d71f065b0a8529e71b5919d93fd
LinqManJr <johnybond32@yandex.ru>
2019-12-16 01:06:55 +0900

    add body of helpers

---
+++ SearchEngine/SearchEngine.WebApp/Helpers/TagHelpers.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SearchEngine.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchEngine.WebApp.Helpers
+{
+    public static class TagHelpers
+    {
+        public static HtmlString CreatePartialList(this IHtmlHelper html,IEnumerable<ItemResult> results)
+        {
+            var tagBuilder = new TagBuilder("div");
+            tagBuilder.AddCssClass("list-group");
+
+            var content = new TagBuilder("div");
+            content.AddCssClass("d-flex w-100 justify-content-between");           
+
+
+            return new HtmlString(tagBuilder.ToString());
+        }
+
+        public static HtmlString CreateErrorItem(this IHtmlHelper html, ErrorItem item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
