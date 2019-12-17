198d11b66719d077a7835d0f52a87c52ccd084ad
LinqManJr <johnybond32@yandex.ru>
2019-12-17 21:36:11 +0900

    complete save action

--- SearchEngine/SearchEngine.WebApp/Controllers/ConfigurationController.cs
+++ SearchEngine/SearchEngine.WebApp/Controllers/ConfigurationController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Configuration;
+using SearchEngine.Core.Configurations;
 
 namespace SearchEngine.WebApp.Controllers
 {
@@ -16,6 +18,7 @@ namespace SearchEngine.WebApp.Controllers
         {
             _configuration = configuration;
         }
+        
         public IActionResult Index()
         {
             var sections = _configuration.GetSection("EnginesConfig").GetChildren().Select(x => x.Key).ToList();
@@ -29,8 +32,24 @@ namespace SearchEngine.WebApp.Controllers
                                         .GetSection(sectionName)
                                         .GetChildren()
                                         .ToDictionary(x => x.Key, y => y.Value);
+            ViewBag.SectionName = sectionName;
 
             return PartialView("_ConfigFields", fields);
+        }
+        [HttpPost]
+        public IActionResult Save(Dictionary<string,string> dict)
+        {
+            var sectionName = dict.First().Value;
+            var engineSection = _configuration.GetSection("EnginesConfig").GetSection(sectionName);
+
+            foreach (var pair in dict.Skip(1))
+            {
+                if (engineSection.GetSection(pair.Key).Exists())
+                {
+                    engineSection[pair.Key] = pair.Value;
+                }
+            }
+            return RedirectToAction("Index");
         }
     }
 }
