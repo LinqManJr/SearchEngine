a6451a80ad7ff12c8ca29e8f172beb4ef50ee1fb
LinqManJr <johnybond32@yandex.ru>
2019-12-18 17:44:06 +0900

    clear json options add NumItems to json

--- SearchEngine/SearchEngine.Tests/DefaultConfigs.cs
+++ SearchEngine/SearchEngine.Tests/DefaultConfigs.cs
@@ -11,23 +11,23 @@ namespace SearchEngine.Tests
         { 
             Name = "yandex", 
             Uri = "https://yandex.com/search/xml", 
-            Apikey = "03.304041461:62374306f8f0c1938a6a26f0ce0511be", 
-            Username = "johnybond32"
+            Apikey = "", 
+            Username = ""
         };
 
         public static GoogleSearchOptions GoogleOptions => new GoogleSearchOptions 
         { 
             Name = "google", 
             Uri = string.Empty,
-            Apikey = "AIzaSyB3ex6PDKCy54J92_1rB0q1TBn1jbv43SU", 
-            AppId = "012320430393294220051:cxnkugrhcjf"            
+            Apikey = "", 
+            AppId = ""            
         };
 
         public static SearchEngineOptions BingOptions => new SearchEngineOptions 
         { 
             Name = "bing", 
             Uri = "https://api.cognitive.microsoft.com/bing/v7.0/search", 
-            Apikey = "2efb912d79e84eb6820192d1805fb44b"
+            Apikey = ""
         };
 
         public static IList<ItemResult> GetDefaultItemsOfResult => new List<ItemResult>
