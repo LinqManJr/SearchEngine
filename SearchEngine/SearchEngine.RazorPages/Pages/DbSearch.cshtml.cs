6a9d129e37aae73c94b26df2f2038700e1cb879c
LinqManJr <johnybond32@yandex.ru>
2019-12-23 12:08:23 +0900

    done with _details partial

--- SearchEngine/SearchEngine.RazorPages/Pages/DbSearch.cshtml.cs
+++ SearchEngine/SearchEngine.RazorPages/Pages/DbSearch.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SearchEngine.Domain.Models;
 using SearchEngine.RazorPages.Services;
@@ -11,7 +12,7 @@ namespace SearchEngine.RazorPages
     {
         private readonly IDatabaseService _dbService;
 
-        public List<string> DropdownWords { get; set; }
+        public List<string> DropdownWords { get; set; }        
 
         public IEnumerable<Request> Requests { get; set; }
         public DbSearchModel(IDatabaseService dbService)
@@ -22,12 +23,18 @@ namespace SearchEngine.RazorPages
 
         public async Task OnGetAsync()
         {            
-            Requests = await _dbService.GetRequests();
+            Requests = await _dbService.GetRequests();            
         }
 
         public async Task OnPostAsync(string filterWord)
         {            
             Requests = await _dbService.GetRequestsByWord(filterWord);            
+        }
+
+        public async Task<IActionResult> OnGetDetails(int id)
+        {
+            var result = await _dbService.GetResultById(id);            
+            return Partial("../Partials/_Details", result);
         }
     }
 }
