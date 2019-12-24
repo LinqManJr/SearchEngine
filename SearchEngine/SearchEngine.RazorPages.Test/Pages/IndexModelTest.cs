eb86e7051a5c6f5240dd35f5b2883e693afceb0f
LinqManJr <johnybond32@yandex.ru>
2019-12-24 17:06:06 +0900

    add IndexmodelTest

---
+++ SearchEngine/SearchEngine.RazorPages.Test/Pages/IndexModelTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using SearchEngine.RazorPages.Pages;
+using SearchEngine.RazorPages.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngine.RazorPages.Test.Pages
+{
+    [TestFixture]
+    public class IndexModelTest
+    {
+        private IndexModel _page;
+        private IDatabaseService _dbService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _dbService = DefaultConfigs.DefaultISearchDbService;
+        }
+
+        [Test]
+        [TestCase("singleton")]
+        public async Task ShouldCheckValidSearchResultOnPostAsync(string searchText)
+        {
+            _page = new IndexModel(DefaultConfigs.GetDefaultSearchService(), _dbService);
+            await _page.OnPostAsync(searchText);
+            Assert.IsNull(_page.SearchResult.Error);
+        }
+
+        [Test]
+        [TestCase("singleton")]
+        public async Task ShouldCheckErrorSearchResultOnPostAsync(string searchText)
+        {
+            _page = new IndexModel(DefaultConfigs.GetBadSearchService(), _dbService);
+            await _page.OnPostAsync(searchText);
+            Assert.IsNotNull(_page.SearchResult.Error);
+        }
+    }
+}
