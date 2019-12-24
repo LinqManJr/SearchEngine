ee532a1ee06a0a7af9835283583de78f1d0d0c1c
LinqManJr <johnybond32@yandex.ru>
2019-12-24 17:03:08 +0900

    fix defaultconfigs

--- SearchEngine/SearchEngine.RazorPages.Test/DefaultConfigs.cs
+++ SearchEngine/SearchEngine.RazorPages.Test/DefaultConfigs.cs
@@ -1,30 +1,77 @@
-﻿using SearchEngine.Core.Configurations;
+﻿using Microsoft.EntityFrameworkCore;
+using SearchEngine.Core.Configurations;
+using SearchEngine.Core.Engines;
+using SearchEngine.Core.Models;
+using SearchEngine.Core.Services;
+using SearchEngine.Domain.Context;
+using SearchEngine.RazorPages.Services;
+using System.Collections.Generic;
 
 namespace SearchEngine.RazorPages.Test
 {
     public static class DefaultConfigs
     {
-        public static YandexSearchOptions YaOptions => new YandexSearchOptions
+        public static IList<ItemResult> GetDefaultItemsOfResult => new List<ItemResult>
         {
-            Name = "yandex",
-            Uri = "https://yandex.com/search/xml",
-            Apikey = "",
-            Username = ""
+            new ItemResult("title1","link1"),
+            new ItemResult("title2","link2"),
+            new ItemResult("title3","link3"),
+            new ItemResult("title4","link4"),
+            new ItemResult("title5","link5"),
+            new ItemResult("title6","link6"),
+            new ItemResult("title7","link7"),
+            new ItemResult("title8","link8"),
+            new ItemResult("title9","link9"),
+            new ItemResult("title10","link10")
         };
 
-        public static GoogleSearchOptions GoogleOptions => new GoogleSearchOptions
-        {
-            Name = "google",
-            Uri = string.Empty,
-            Apikey = "",
-            AppId = ""
-        };
+        public static ErrorItem GetDefaultErrorItem => new ErrorItem("BadRequest", "Something went wrong");
 
-        public static SearchEngineOptions BingOptions => new SearchEngineOptions
+        public static ISearchService GetDefaultSearchService()
         {
-            Name = "bing",
-            Uri = "https://api.cognitive.microsoft.com/bing/v7.0/search",
-            Apikey = ""
+            var googleOptions = new GoogleSearchOptions
+            {
+                Name = "google",
+                Uri = string.Empty,
+                Apikey = "",
+                AppId = ""
+            };
+
+            return new SearchService(new List<ISearchEngine> { new GoogleSearchEngine(googleOptions) });
+        }
+
+        public static ISearchService GetBadSearchService()
+        {
+            var googleOptions = new GoogleSearchOptions
+            {
+                Name = "google",
+                Uri = string.Empty,
+                Apikey = "",
+                AppId = ""
+            };
+
+            return new SearchService(new List<ISearchEngine> { new GoogleSearchEngine(googleOptions) });
+        }
+
+        public static IDatabaseService DefaultISearchDbService => new SearchDbService(DefaultContext);
+        public static IDatabaseService GetDefaultServiceWithContext(SearchContext context)
+        {
+            return new SearchDbService(context);
+        }
+        public static SearchContext DefaultContext
+        {
+            get
+            {
+                return new SearchContext(new DbContextOptionsBuilder<SearchContext>().UseInMemoryDatabase("SearchDb").Options);
+            }
+        }
+
+        public static SearchResult DefaultSearchResult => new SearchResult
+        {
+            CountResult = 10000,
+            SearchTitle = "word",
+            Error = null,
+            Results = GetDefaultItemsOfResult
         };
     }
 }
