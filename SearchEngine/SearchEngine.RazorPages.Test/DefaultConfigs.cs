3c55fb3e0eadef4ea2b8f3d3da628e89e1bb63ba
LinqManJr <johnybond32@yandex.ru>
2019-12-24 16:57:43 +0900

    add folders pages, services add defaultconfigs

---
+++ SearchEngine/SearchEngine.RazorPages.Test/DefaultConfigs.cs
@@ -0,0 +1,30 @@
+﻿using SearchEngine.Core.Configurations;
+
+namespace SearchEngine.RazorPages.Test
+{
+    public static class DefaultConfigs
+    {
+        public static YandexSearchOptions YaOptions => new YandexSearchOptions
+        {
+            Name = "yandex",
+            Uri = "https://yandex.com/search/xml",
+            Apikey = "",
+            Username = ""
+        };
+
+        public static GoogleSearchOptions GoogleOptions => new GoogleSearchOptions
+        {
+            Name = "google",
+            Uri = string.Empty,
+            Apikey = "",
+            AppId = ""
+        };
+
+        public static SearchEngineOptions BingOptions => new SearchEngineOptions
+        {
+            Name = "bing",
+            Uri = "https://api.cognitive.microsoft.com/bing/v7.0/search",
+            Apikey = ""
+        };
+    }
+}
