f300561a33027ec7100cd1b5b42a40ce4aff77e2
LinqManJr <johnybond32@yandex.ru>
2019-12-24 17:08:05 +0900

    add DbSearchModelTest

---
+++ SearchEngine/SearchEngine.RazorPages.Test/Pages/DbSearchModelTest.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using SearchEngine.Core.Models;
+using SearchEngine.RazorPages.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngine.RazorPages.Test.Pages
+{
+    [TestFixture]
+    public class DbSearchModelTest
+    {
+        private DbSearchModel _page;
+        private IDatabaseService _dbService;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            _dbService = DefaultConfigs.DefaultISearchDbService;
+            await AddSearchResult(DefaultConfigs.DefaultSearchResult, 5);
+        }
+
+        private async Task AddSearchResult(SearchResult result, int count)
+        {
+            foreach (var i in Enumerable.Range(0, count))
+                await _dbService.AddRequestToDb(result, $"word{i}");
+        }
+
+        [Test]
+        public void ShouldReturnDropdownWords()
+        {
+            _page = new DbSearchModel(_dbService);
+            var words = _page.DropdownWords;
+            Assert.IsTrue(words.Count > 0); ;
+        }
+
+        [Test]
+        public async Task ShouldReturnRequestsOnGetAsync()
+        {
+            _page = new DbSearchModel(_dbService);
+            await _page.OnGetAsync();
+            Assert.IsNotNull(_page.Requests);
+            Assert.IsTrue(_page.Requests.Count() > 0);
+        }
+
+        [Test]
+        public async Task ShouldReturnFilterByWordRequests()
+        {
+            _page = new DbSearchModel(_dbService);
+            await _page.OnPostAsync("word1");
+            var result = _page.Requests;
+            Assert.That(result.Count() == 1);
+        }
+
+        [Test]
+        public async Task ShouldReturnZeroCountFilterByWordRequests()
+        {
+            _page = new DbSearchModel(_dbService);
+            await _page.OnPostAsync("sometext");
+            var result = _page.Requests;
+            Assert.That(result.Count() == 0);
+        }
+    }
+}
